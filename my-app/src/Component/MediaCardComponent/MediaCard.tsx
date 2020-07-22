e8bedb2ff9ba80d5119a3ff8e24383a947d1973f
Daphne <dsmhan7@gmail.com>
2020-07-22 21:35:30 +1200

    adding card component

---
+++ my-app/src/Component/MediaCardComponent/MediaCard.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import './MediaCard.css';
+
+interface IMediaCardProps {
+    ImageUrl: string | undefined;
+}
+
+function MediaCard(props: IMediaCardProps) {
+    return (
+        <div>
+            <Card className="MediaCardContainer">
+                <CardActionArea>
+                    <CardMedia
+                        className="MediaCardImage"
+                        image={props.ImageUrl}
+                    />
+                    <CardContent/>
+                </CardActionArea>
+            </Card>
+        </div>
+    )
+}
+
+export default MediaCard
